46d40d7be9ba627e361ba0f87c51d315ddadfb82
wo1sgier <gw4133@mci4me.at>
2025-12-19 11:25:44 +0100

    Neue Nutzerfunktion

---
+++ Klassen.py
@@ -0,0 +1,19 @@
+
+
+class User:
+
+    def __init__(self, username, email, userid):
+        self.username = username
+        self.email = email
+        self.userid = userid
+    
+
+class Device:
+    def __init__(self, Geraetename, Geraetefarbe,):
+        self.name = Geraetename
+        self.farbe = Geraetefarbe
+    
+        
+
+
+    
--- Verwaltung/Nutzerverwaltung.py
+++ Verwaltung/Nutzerverwaltung.py
@@ -3,3 +3,9 @@ import streamlit as st
 def app():
     st.write("# Nutzer-Verwaltung")
     st.write("## Nutzer anlegen")
+
+    with st.form("create_user_form"):
+        name  = st.text_input("Name")
+        email = st.text_input("E-Mail")
+
+        submitted = st.form_submit_button("User erstellen")
